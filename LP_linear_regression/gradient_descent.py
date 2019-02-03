2b2906aac6f691f45f005f1155da368425013ceb
Geoff deRosenroll <geoffderosenroll@gmail.com>
2019-02-03 13:22:50 -0800

    de-linted

--- LP_linear_regression/gradient_descent.py
+++ LP_linear_regression/gradient_descent.py
@@ -1,17 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 10 # data points
-D = 3 # dimensionality
+N = 10   # data points
+D = 3  # dimensionality
 
 # initialize X
-X = np.zeros((N, D)) # (N, D) is one argument, that's why 2 brackets
-X[:,0] = 1 # bias term
-X[:5, 1] = 1 # first 5 ele of first column to 1
-X[5:, 2] = 1 # last five elements of the third column to 1
+X = np.zeros((N, D))  # (N, D) is one argument, that's why 2 brackets
+X[:, 0] = 1  # bias term
+X[:5, 1] = 1  # first 5 ele of first column to 1
+X[5:, 2] = 1  # last five elements of the third column to 1
 
 # initialize Y
-Y = np.array([0]*5 + [1]*5) # [0,0,0,0,0,1,1,1,1,1]
+Y = np.array([0]*5 + [1]*5)  # [0,0,0,0,0,1,1,1,1,1]
 
 # if you try to do the usual w solution it fails
 try:
@@ -21,16 +21,18 @@ except Exception as e:
 
 # instead we will use gradient descent to calculate w
 costs = []
-w = np.random.randn(D) / np.sqrt(D) # mu = 0, variance = 1/D
+w = np.random.randn(D) / np.sqrt(D)  # mu = 0, variance = 1/D
 print("initial weights:", w)
 learning_rate = .001
 tries = 1000
 
+
 def costDeriv(w):
     return X.T @ (X @ w - Y)
-    #return X.T @ (X @ w) - X.T @ Y
+    # return X.T @ (X @ w) - X.T @ Y
 
-#print((X.T @ (X @ w - Y)) == (X.T @ (X @ w) - X.T @ Y))
+# print((X.T @ (X @ w - Y)) == (X.T @ (X @ w) - X.T @ Y))
+
 
 for i in range(tries):
     w = w - learning_rate * costDeriv(w)
