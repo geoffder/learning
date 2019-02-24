f65db8efa5d5dc833682c744eb55dbc2524abcbc
Geoff deRosenroll <geoffder@uvic.ca>
2019-02-24 01:56:36 -0800

    de-linted

--- LP_logistic_regression/logistic_train.py
+++ LP_logistic_regression/logistic_train.py
@@ -5,29 +5,35 @@ from sklearn.utils import shuffle
 from process import get_binary_data
 
 X, Y = get_binary_data()
-X, Y = shuffle(X, Y) # shuffle the order of samples
+X, Y = shuffle(X, Y)  # shuffle the order of samples
 # then split the shuffled samples between train and test sets
-Xtrain = X[:-100] # everything but the last 100 for train
+Xtrain = X[:-100]  # everything but the last 100 for train
 Ytrain = Y[:-100]
-Xtest = X[-100:] # the last 100 samples for test
+Xtest = X[-100:]  # the last 100 samples for test
 Ytest = Y[-100:]
 
-D = X.shape[1] # reminder: ones column was not added in process.py
+D = X.shape[1]  # reminder: ones column was not added in process.py
 W = np.random.randn(D) / np.sqrt(D)
 b = 0
+
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
+
 def forward(X, W, b):
     return sigmoid(X @ W + b)
+
 
 def classification_rate(Y, P):
     return np.mean(Y == P)
 
+
 def cross_entropy(T, pY):
-    #return -(T * np.log(Y) + (1 - T)*np.log(1 - Y)).sum() # it was just sum before..
-    return -np.mean(T*np.log(pY) + (1 - T)*np.log(1 - pY)) # mean instead of sum?
+    # it was just sum before..
+    # return -(T * np.log(Y) + (1 - T)*np.log(1 - Y)).sum()
+    return -np.mean(T*np.log(pY) + (1 - T)*np.log(1 - pY))  # mean instead
+
 
 train_costs = []
 test_costs = []
@@ -45,8 +51,10 @@ for i in range(1000):
     if i % 1000 == 0:
         print(i, cross_entropy(Ytrain, pYtrain), cross_entropy(Ytest, pYtest))
 
-print("Final train classification rate:", classification_rate(Ytrain, np.round(pYtrain)))
-print("Final test classification rate:", classification_rate(Ytest, np.round(pYtest)))
+print("Final train classification rate:",
+      classification_rate(Ytrain, np.round(pYtrain)))
+print("Final test classification rate:",
+      classification_rate(Ytest, np.round(pYtest)))
 
 legend1, = plt.plot(train_costs, label='train costs')
 legend2, = plt.plot(test_costs, label='test costs')
