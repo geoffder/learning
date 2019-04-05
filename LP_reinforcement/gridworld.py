b8c6f67e9d40e9403c075493af6d1d1462c45a5a
Geoff deRosenroll <geoffderosenroll@gmail.com>
2019-04-04 17:27:20 -0700

    Monte Carlo solution to plain and windy gridworld

--- LP_reinforcement/gridworld.py
+++ LP_reinforcement/gridworld.py
@@ -1,4 +1,4 @@
-# import numpy as np
+import numpy as np
 
 """
 Similar to Lazy Programmer's, but not exactly. I'll see how much sense it
@@ -9,11 +9,14 @@ agents that will interact with this environment.
 
 class Grid(object):
 
-    def __init__(self, dims, walls, terminals, start, step_cost=0):
+    def __init__(self, dims, walls, terminals, start, step_cost=0,
+                 windy=False):
         # basic layout and rules
         self.height, self.width = dims
         self.walls = set(walls)  # list of coordinate tuples converted to set
         self.moves = {'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1)}
+        self.move_list = ['U', 'D', 'L', 'R']
+        self.windy = windy  # non-deterministic state transitions
         # states and actions
         self.terminals = set(terminals.keys())  # terminal positions
         self.rewards = terminals  # rewards initialized with terminal states
@@ -21,12 +24,19 @@ class Grid(object):
         self.actions = {}  # dict to be filled {(i, j): [actions]}
         self.enumerate_states()  # fill action and reward dicts
         # agent (start) position
+        self.start = start
         self.last_pos = start
         self.i, self.j = start
 
     def move(self, action):
         "Move Up, Down, Left, or Right (if legal, otherwise do nothing)"
         self.last_pos = (self.i, self.j)  # for undo-ing moves
+
+        if self.windy:
+            # half of the time get blown in one of the other directions
+            prob = [.5/3 if i != self.move_list.index(action) else .5
+                    for i in range(4)]
+            action = np.random.choice(self.move_list, p=prob)
 
         if action in self.actions.get(self.last_pos, []):
             self.i += self.moves[action][0]
@@ -42,6 +52,9 @@ class Grid(object):
 
     def get_state(self):
         return (self.i, self.j)
+
+    def reset(self):
+        self.i, self.j = self.start
 
     def is_terminal(self, s):
         "Check if given state is terminal."
@@ -98,13 +111,14 @@ class Grid(object):
         print("-" + "-------"*self.width, end='\n\n')
 
 
-def standard_grid(step_cost=0):
+def standard_grid(step_cost=0, windy=False):
     # .  .  .  1
     # .  x  . -1
     # s  .  .  .
     terminals = {(0, 3): 1, (1, 3): -1}
     walls = [(1, 1)]
-    the_grid = Grid((3, 4), walls, terminals, (2, 0), step_cost=step_cost)
+    the_grid = Grid((3, 4), walls, terminals, (2, 0), step_cost=step_cost,
+                    windy=windy)
     return the_grid
 
 
---
+++ LP_reinforcement/monte_carlo.py
@@ -0,0 +1,146 @@
+import numpy as np
+from gridworld import standard_grid
+
+ALL_ACTIONS = ['U', 'D', 'L', 'R']
+
+
+class MonteCarloPolicy(object):
+
+    def __init__(self, alpha=.9, gamma=.9, epsilon=.1, sample_mean=False):
+        # state -> action policy, instead of initializing, I'll take a
+        # discovering states as I go approach
+        self.actions = {}
+        self.V = {}  # state-values (add states as game is played)
+        self.Q = {}  # values of state-action pairs
+        self.sample_mean = sample_mean  # use sample mean vs moving average
+        self.N = {}  # number of samples of state-action pairs
+        self.alpha = alpha  # moving sample mean decay
+        self.gamma = gamma  # discount ratio of future returns
+        self.epsilon = epsilon  # chance to explore (epsilon-greedy)
+
+    def display_values(self, grid):
+        """
+        Printing out the values (expected returns) of each place on the grid
+        according to this policy.
+        """
+        for i in range(grid.height):
+            print("-" + "-------"*grid.width)
+            for j in range(grid.width):
+                if not j:
+                    print("|", end="")  # begin row with vertical line
+                v = self.V.get((i, j), 0)
+                if np.isnan(v):
+                    print("     |", end="")
+                elif v >= 0:
+                    print(" %.2f |" % v, end="")
+                else:
+                    print("%.2f |" % v, end="")  # -ve sign takes extra space
+            print("")  # new line
+        print("-" + "-------"*grid.width, end='\n\n')
+
+    def display(self, grid):
+        """
+        Printing out the actions that will be taken at each place on the grid,
+        according to this policy.
+        """
+        for i in range(grid.height):
+            print("-" + "-------"*grid.width)
+            for j in range(grid.width):
+                if not j:
+                    print("|", end="")  # begin row with vertical line
+                a = self.actions.get((i, j), ' ')
+                print("   %s  |" % a, end="")
+            print("")  # new line
+        print("-" + "-------"*grid.width, end='\n\n')
+
+    def play_episode(self, grid):
+        grid.reset()  # reset agent to start position
+        history = []
+        while not grid.gameover():
+            s = grid.get_state()
+            if np.random.random() > self.epsilon:
+                # if no policy yet for current state, behave randomly
+                a = self.actions.get(s, np.random.choice(ALL_ACTIONS))
+            else:
+                a = np.random.choice(ALL_ACTIONS)
+            r = grid.move(a)  # take action and get reward
+            history.append((s, a, r))
+
+        return self.calculate_returns(history)
+
+    def calculate_returns(self, history):
+        G = 0  # return of terminal state is zero
+        s_a_returns = []  # returns of state-action pairs
+        for s, a, r in reversed(history):
+            G = r + self.gamma*G  # return for this state-action pair
+            s_a_returns.append((s, a, G))
+
+        return reversed(s_a_returns)  # forward chronological order
+
+    def calculate_state_values(self):
+        visited = set()
+        for (s, _) in self.Q.keys():
+            if s not in visited:
+                visited.add(s)
+                self.V[s] = np.max(
+                    [self.Q.get((s, a), np.NaN) for a in ALL_ACTIONS])
+
+    def evaluation(self, grid, N, first_visit=True):
+        visited = set()
+        for _ in range(N):
+            s_a_returns = self.play_episode(grid)
+            for s, a, g in s_a_returns:
+                if (s, a) not in visited or not first_visit:
+                    visited.add((s, a))  # don't count again
+                    old_q = self.Q.get((s, a), np.random.random())
+                    if self.sample_mean:
+                        # use sample mean to estimate Q
+                        self.N[(s, a)] = self.N.get((s, a), 0) + 1
+                        self.Q[(s, a)] = (
+                            1 - 1/self.N[(s, a)])*old_q + g/self.N[(s, a)]
+                    else:
+                        # use exponential moving average to estimate Q
+                        self.Q[(s, a)] = (1 - self.alpha)*old_q + self.alpha*g
+            visited.clear()  # wipe for next episode
+
+    def update(self):
+        changed = False  # track whether policy has changed (for return)
+        visited = set()
+        for (s, _) in self.Q.keys():
+            if s not in visited:
+                visited.add(s)
+                old_a = self.actions.get(s, 0)  # keep track (check for change)
+                options = [self.Q.get((s, a), np.NaN) for a in ALL_ACTIONS]
+                self.actions[s] = ALL_ACTIONS[np.argmax(options)]
+                changed = True if self.actions[s] != old_a else changed
+        return changed
+
+    def improve(self, grid, first_visit=True):
+        iterations = 0
+        changed = True
+        while changed or (iterations < 750 and grid.windy):
+            self.evaluation(grid, 1, first_visit=first_visit)
+            changed = self.update()
+            iterations += 1
+        return iterations
+
+
+if __name__ == '__main__':
+    the_grid = standard_grid(step_cost=-0.1, windy=True)
+
+    # print rewards associated with transitioning into each state on the grid
+    print("Rewards:")
+    the_grid.display_rewards()
+
+    # create MC policy object. Action policy and Values not initialized
+    policy = MonteCarloPolicy(alpha=.9, gamma=.9, sample_mean=True)
+
+    # run until policy is unchanging (if windy, run a minimum number of times)
+    iterations = policy.improve(the_grid, first_visit=False)
+    policy.calculate_state_values()  # calculate values of each state from Q
+
+    print("Learned values (after %d iterations):" % iterations)
+    policy.display_values(the_grid)
+
+    print("Learned policy (after %d iterations):" % iterations)
+    policy.display(the_grid)
