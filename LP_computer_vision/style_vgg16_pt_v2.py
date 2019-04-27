1f44ae879edf8ec918e3436d25f1e0db7a2acbca
Geoff deRosenroll <geoffder@uvic.ca>
2019-04-26 20:18:43 -0700

    some additional touches, and upgrade to v2
    Now properly using batchnorm, colours are noticeably better

--- LP_computer_vision/style_vgg16_pt_v2.py
+++ LP_computer_vision/style_vgg16_pt_v2.py
@@ -18,29 +18,18 @@ def Gram(X):
     return (X @ X.t()) / X.numel()
 
 
-class StyleTransferLoss(torch.autograd.Function):
+class StyleTransferLoss(nn.Module):
+    """
+    Custom function that combines Content Loss (MSE between source content
+    and contstruct convolutional representation from a particular layer) and
+    Style Loss (MSE between Gram matrices [Features x Features] of source style
+    and construct convolutional representations from a set of layers).
 
+    alpha and beta are used to weight the importance of content and style loss
+    respectively.
+    """
     def __init__(self, alpha=.5, beta=.5):
         super(StyleTransferLoss, self).__init__()
-        self.alpha = alpha
-        self.beta = beta
-
-    def forward(self, X_content, X_style, content_out, style_out):
-        content_loss = torch.mean((X_content - content_out)**2)
-        style_loss = torch.sum(torch.stack(
-            [torch.sum((Gram(x) - Gram(s))**2)
-             for x, s in zip(X_style, style_out)]
-        ))
-        # print("content loss:", content_loss.data,
-        #       "style loss:", style_loss.data)
-        output = self.alpha*content_loss + self.beta*style_loss
-        return output
-
-
-class StyleTransferLossMod(nn.Module):
-
-    def __init__(self, alpha=.5, beta=.5):
-        super(StyleTransferLossMod, self).__init__()
         self.alpha = alpha
         self.beta = beta
 
@@ -56,35 +45,37 @@ class StyleTransferLossMod(nn.Module):
 
 class StyleVGG16(object):
     """
-    Trying out tranfer learning with VGG16. This class uses the same structure
-    I have used for my own networks, but instead uses a pre-trained build of
-    the VGG network with the relevant components switched out to fit the data.
+    Style-transfer using pre-trained VGG16 network.
     """
-    def __init__(self, content_layer=17, style_layers=[0, 5, 10, 17, 24]):
-        self.model = self.build()
-        self.model.to(device)
+    def __init__(self, content_layer=17, style_layers=[0, 5, 10, 17, 24],
+                 batchnorm=False):
+        self.model = self.build(batchnorm)
+        self.model.to(device).eval()  # send to GPU and set to evaluation mode
         self.content_layer = content_layer
         self.style_layers = set(style_layers)  # style outputs
 
-    def build(self):
+    def build(self, batchnorm):
         # load pre-trained vgg (only use convolutional layers)
-        vgg = models.vgg16(pretrained=True)
-        # vgg = models.vgg16_bn(pretrained=True)
+        if not batchnorm:
+            vgg = models.vgg16(pretrained=True)  # with batchnorm
+        else:
+            vgg = models.vgg16_bn(pretrained=True)  # with batchnorm
+
         # take conv layers out of the nn.Sequential that holds them
         vgg_conv = nn.ModuleList(
             list(list(vgg.children())[0].children())[:-1]
         )
 
-        # freeze covolutional layers
-        for layer in vgg_conv.children():
-            for param in layer.parameters():
-                param.requires_grad = False
+        # freeze covolutional and batchnorm layers
+        for param in vgg_conv.parameters():
+            param.requires_grad = False
 
-        # change all max pooling layers to avg pooling
+        # change all maxpool layers to avgpool and make ReLU not inplace.
         for i, layer in enumerate(vgg_conv.children()):
             if isinstance(layer, nn.MaxPool2d):
                 vgg_conv[i] = nn.AvgPool2d(2, stride=2, padding=0)
             elif isinstance(layer, nn.ReLU):
+                # not using sequential, so inplace would not work as intended
                 vgg_conv[i] = nn.ReLU(inplace=False)
 
         return vgg_conv
@@ -97,21 +88,22 @@ class StyleVGG16(object):
         """
         X_style, style_out = [], []
         for i, layer in enumerate(self.model.children()):
+            # transform X, content, and style with forward() of current layer
             X = layer(X)
             content = layer(content)
             style = layer(style)
+            # store convolutional representations of target layers
             if i in self.style_layers:
                 X_style.append(X)
-                style_out.append(style.detach())
+                style_out.append(style.detach())  # not needed in graph
             if i == self.content_layer:
-                content_out = content.detach()
+                content_out = content.detach()    # not needed in graph
                 X_content = X
 
         return X_content, X_style, content_out, style_out
 
-    def generate(self, content, style, alpha=.5, beta=.5, lr=1e-3, epochs=10,
-                 print_every=50, lbfgs=False):
-        self.lbfgs = lbfgs  # whether to use L-BFGS optimizer
+    def generate(self, content, style, alpha=.5, beta=.5, lr=1, epochs=10,
+                 print_every=50):
 
         # re-shape images for PyTorch
         content = content.reshape(1, *content.shape)
@@ -121,22 +113,19 @@ class StyleVGG16(object):
         self.content = torch.from_numpy(content).float().to(device)
         self.style = torch.from_numpy(style).float().to(device)
 
-        # self.loss = StyleTransferLoss(alpha=alpha, beta=beta)
-        self.loss = StyleTransferLossMod(alpha=alpha, beta=beta).to(device)
-        if not lbfgs:
-            self.optimizer = optim.Adam([self.X], lr=lr)
-        else:
-            self.optimizer = optim.LBFGS([self.X], lr=1)
+        self.loss = StyleTransferLoss(alpha=alpha, beta=beta).to(device)
+        self.optimizer = optim.LBFGS([self.X], lr=lr)
 
         costs = []
         for i in range(epochs):
-            self.X.data.clamp_(0, 1)  # clip to float image range 0 -> 1
-            cost = self.paint()
+            # clip to float image range 0 -> 1
+            self.X.data.clamp_(0, 1)
+            # execute round of L-BFGS optimization with self.paint as closure()
+            cost = self.optimizer.step(self.paint)
+            # print progress
             if i % print_every == 0:
                 print("epoch: %d, cost: %f" % (i, cost))
             costs.append(cost)
-            if lbfgs:
-                self.optimizer.step(self.paint)
         print('')  # line break
 
         painting = self.X.detach().cpu().numpy().reshape(*content.shape[1:])
@@ -149,7 +138,7 @@ class StyleVGG16(object):
         content = content.reshape(*content.shape[1:])
         style = style.reshape(*style.shape[1:])
 
-        # plot cost and accuracy progression
+        # plot original content, style, cost progression, and created image
         fig, axes = plt.subplots(2, 2)
         axes[0, 0].imshow(content.transpose(1, 2, 0), interpolation='bessel')
         axes[0, 1].imshow(style.transpose(1, 2, 0), interpolation='bessel')
@@ -157,13 +146,16 @@ class StyleVGG16(object):
         axes[1, 0].set_xlabel('Epoch')
         axes[1, 0].set_ylabel('Cost')
         axes[1, 1].imshow(painting.transpose(1, 2, 0), interpolation='bessel')
-        # fig.tight_layout()
         plt.show()
 
         return painting
 
     def paint(self):
-        self.model.train()  # set the model to testing mode
+        """
+        Closure function for L-BFGS optimizer. Note the lack of
+        optimizer.step() here as there would be with SGD optimizers. Instead,
+        this function is to optimizer.step() in the training/optimization loop.
+        """
         self.optimizer.zero_grad()  # Reset gradient
 
         # Forward
@@ -173,10 +165,8 @@ class StyleVGG16(object):
 
         # Backward
         output.backward()
-        if not self.lbfgs:
-            self.optimizer.step()  # Update parameters
 
-        return output.item()
+        return output.item()  # cost
 
 
 def main():
@@ -191,11 +181,10 @@ def main():
     style = np.array(style).transpose(2, 0, 1) / 255
 
     # build network
-    vgg = StyleVGG16(content_layer=10)
+    vgg = StyleVGG16(content_layer=17, batchnorm=True)
     # transer style
     painting = vgg.generate(
-        content, style, alpha=1, beta=5, lr=1e-1, epochs=5, print_every=2,
-        lbfgs=True
+        content, style, alpha=1, beta=400, lr=1, epochs=5, print_every=2
     )
     painting = (painting.transpose(1, 2, 0)*255).astype(np.uint8)
     img = Image.fromarray(painting)
